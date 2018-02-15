6bc6a1ffe9e25a242a74c560fa8246723c6d914c
Isley Gao <isley.gao@macys.com>
2018-02-15 13:36:15 -0800

    add all paths method

--- path.py
+++ path.py
@@ -1,14 +1,16 @@
-def find_path(graph, start, end, path=[]):
+def find_all_paths(graph, start, end, path=[]):
   path = path + [start]
   if start == end:
-    return path
-  if start not in graph.keys():
-    return None
+    return [path]
+  if start not in graph:
+    return []
+  paths = []
   for node in graph[start]:
     if node not in path:
-      newpath = find_path(graph, node, end, path)
-      if newpath: return newpath
-  return None
+      newpaths = find_all_paths(graph, node, end, path)
+      for newpath in newpaths:
+        paths.append(newpath)
+  return paths
 
 
 
@@ -19,5 +21,21 @@ graph = {'A': ['B', 'C'],
          'E': ['F'],
          'F': ['C']}
          
-print (find_path(graph, "A", "D"))
+print (find_all_paths(graph, "A", "D"))
 
+def find_shortest_path(graph, start, end, path=[]):
+  path = path + [start]
+  if start == end:
+    return path
+  if start not in graph:
+    return None
+  shortest = None
+  for node in graph[start]:
+    if node not in path:
+      newpath = find_shortest_path(graph, node, end, path)
+      if newpath:
+        if not shortest or (len(newpath) < len(shortest)):
+          shortest = newpath
+  return shortest
+
+print (find_shortest_path(graph, "A", "D"))
