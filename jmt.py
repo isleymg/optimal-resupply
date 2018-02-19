089d40be5bb77e0e04feb181f72b8b9b68e72dc2
Isley Gao <isley.gao@macys.com>
2018-02-19 13:29:16 -0800

    list all waypoints, exit routes, and alt routes

--- jmt.py
+++ jmt.py
@@ -30,13 +30,16 @@ def getElevation(lat, lon):
   '''gets elevation from a given lat and lon string using Google Maps API'''
   r = requests.get("https://maps.googleapis.com/maps/api/elevation/json?locations="+lat+","+lon+"&key="+API_KEY)
   r = r.json()
+  print(r)
   return r['results'][0]['elevation']
 
 
-file = open('jmt-map.gpx')
-gpx = gpxpy.parse(file)
+# file = open('jmt-map.gpx')
+file = open('writegpx.gpx')
 
+jmt_gpx = gpxpy.parse(file)
 
+'''
 print("TRACKS")
 print("------------------------------------------")
 trackpt_count = 0
@@ -68,7 +71,118 @@ print ("\nSTATISTICS")
 print("------------------------------------------")
 print("Trackpoints: " + str(trackpt_count))
 print("Waypoints: " + str(waypt_count))
+'''
+
+# Creating a new file:
+# --------------------
+'''
+Goal: Single GPX file with the following--
+***Waypoints of main JMT markers***
+- Mount Whitney Trail
+- Donohue Pass
+- Island Pass
+- Silver Pass
+- Selden Pass
+- Muir Pass
+- Mather Pass
+- Pinchot Pass
+- Glen Pass
+- Forester Pass
+- Purple Lake
+- Reds Meadow
+- Tuolomne Meadows Trail
+- Cathedral Pass
+- Sunrise Camp
+- Lyell Fork Base Trail
+- Wallace Creek
+- Woods Creek
+- Taboose Pass Trail
+- Palisade Creek
+- Blaney Meadows Trail
+- Bear Creek Trail
+- Bear Ridge
+- Mono Creek Bridge
+- Johnson Meadow
+- Rosalie Lake
+- Garnet Lake
+- Thousand Island Lake
+- Tuolomne Meadows Trail
+- Halfdome Trail
+- Happy Isle
+- Whitney Summit
+
+***Exit Routes to leave the trail and/or resupply***
+- Exit - Bear Ridge Trail
+- Exit - Bishop Pass Trail
+- Exit - Bullfrog Lake Trail
+- Exit - Horseshoe Lake Trail
+- Exit - Kearsarge Trail
+- Exit - Mammoth Pass Trail
+- Exit - Reds Meadow Trail
+- Exit - Muir Ranch Trail
+- Exit - VVR Trail
+- Exit - Tuolumne Spur
+- Exit - Tuolumne Spur 2
+- 
+
+***Alternate Routes***
+- Alt - Devils Postpile Alternate
+- Alt - Evolution Creek Alternate
+- Alt - Muir Ranch Alternate
+- Alt - Whitney Portal Trail
+
+***Main Trail Routes***
+- 05 - Tuolumne CG Trail
+- 03 - Whitney Trail
+- 06 - Yosemite Valley Spur Trail
+- 07 - Yosemite Valley Trail
+- 01 - Whitney Portal Trail
+- 04 - JMT - Main
+- 02 - Mountaineers Route
+
+***Segments between each waypoint***
 
 
+
+'''
+
+gpx = gpxpy.gpx.GPX()
+
+# Add elevation to track points
+for track in jmt_gpx.tracks:
+  for segment in track.segments:
+    for point in segment.points:
+      try:
+        # elevation = getElevation(str(point.latitude), str(point.longitude))
+        #Added methods into gpx.py to allow for adding elevation
+        elevation = 4000
+        point.set_elevation(elevation)
+      except Exception as e:
+        print(e)
+        print ('Trackpoint at ({0:7f},{1:7f}) -> Elevation: {2}'.format(point.latitude, point.longitude, point.elevation))
+  gpx_track = gpxpy.gpx.GPXTrack()
+  gpx.tracks.append(gpx_track)
+
+print('done')
+
+
+
+# Create first segment in our GPX track:
+
+for route in gpx.routes:
+  print ('Route:')
+  for point in route.points:
+    print ('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+  gpx_segment = gpxpy.gpx.GPXTrackSegment()
+  gpx_track.segments.append(gpx_segment)
+
+# Create points:
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1234, 5.1234, elevation=1234))
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1235, 5.1235, elevation=1235))
+gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(2.1236, 5.1236, elevation=1236))
+
+# You can add routes and waypoints, too...
+
+print ('Created GPX:' +  gpx.to_xml())
 
 
