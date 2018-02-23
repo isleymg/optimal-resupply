58b3802cb7c35cee32d9910e42912d4334aea061
Isley Gao <isley.gao@macys.com>
2018-02-22 20:22:31 -0800

    added lat/long/elevation for node tuple

--- jmt.py
+++ jmt.py
@@ -5,42 +5,35 @@ Creates a graph representation of the points from cleaned_data.gpx
 from graphs import Digraph, Node
 import gpxpy
 
-file = open('test_data.gpx')
+file = open('test2.gpx')
 jmt_gpx = gpxpy.parse(file)
 gpxObj = gpxpy.gpx.GPX()
 
 
 digraph = Digraph()
 
-'''add each track point to digraph'''
-
+'''get starting track point'''
 for track in jmt_gpx.tracks:
   for segment in track.segments:
     for point in segment.points:
-      start_node = Node(point.latitude, point.longitude, point.elevation, None)
-      digraph.add_node(start_node)
-      break
-
-print(start_node)
+        from_node = digraph.add_node(Node((point.latitude, point.longitude, point.elevation)))
+        break
 
 
 '''add edge and weight (elevation difference) between each track point'''
-prev_node = start_node
+to_node = None
 for track in jmt_gpx.tracks:
   for segment in track.segments:
     for point in segment.points:
-      digraph.add_edge(prev_node, point, 2)
-      prev_node = point
+        to_node = Node((point.latitude, point.longitude, point.elevation))
+        digraph.add_edge(from_node, to_node)
+        from_node = to_node
 
-print(prev_node)
-
-
-digraph.get_stats()
-digraph.print_nodes()
-# for i in digraph.get_parents():
-#   print(i)
+## POR QUE NODE CONNECTIONS RETURNING EMPTY?!
+for node in digraph.node_list:
+    print(node.connected_to)
+# digraph.show()
 
 
 # TODO: 
 # 1) modify Digraph class to print visual of nodes
-# 2) modify node data structure to allow for name/lat-long/elevation
