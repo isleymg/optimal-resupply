24aca3a6db311b56b1135876209d39dc9e684038
Isley Gao <isley.gao@macys.com>
2018-02-27 16:49:58 -0800

    handle case for no elevation data

--- jmt.py
+++ jmt.py
@@ -5,6 +5,7 @@ Creates a graph representation of the points from cleaned_data.gpx
 from graphs import Digraph, Node
 import gpxpy
 
+# use 'jmt.gpx' for actual JMT coordinate points and waypoints
 file = open('test3.gpx')
 jmt_gpx = gpxpy.parse(file)
 gpxObj = gpxpy.gpx.GPX()
@@ -32,8 +33,23 @@ for track in jmt_gpx.tracks:
         if to_node.id == from_node.id:
             pass
         else:
-            weight = to_node.id[2]-from_node.id[2]
+            if (from_node.id[2] == None) or (to_node.id[2] == None):
+                weight = 0
+            else:
+                weight = to_node.id[2]-from_node.id[2]
             digraph.add_edge(from_node, to_node, weight)
             from_node = to_node
 
 digraph.show()
+
+
+'''
+TODO: 
+1) Determine function for weight:
+    - elevation gain/loss
+    - water sources
+2) Add/modify nodes for Waypoints
+    - resupply locations
+    - non-resupply locations
+3) Add tracks for alternate trails to resupply locations
+'''
